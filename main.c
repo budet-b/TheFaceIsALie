977bc9d4c3523e46fb5e8932830ef566f9d80ed9
Loic Reyreaud <loic.reyreaud@epita.fr>
2015-10-12 09:10:08 +0200

    LACONDETARACE

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "haar.c"
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+
+
+int main(int argc, char* argv[]) {
+	printf("THE FACE IS A LIE\n");
+	if (argc < 2)
+    		errx(2, "Usage:\n%s <path>", argv[0]);
+	SDL_Surface* image = load_image(argv[1]);
+	ToGray(image);
+    Binarize(image);
+    //int** integralMatrix = matrix_integralImage(image);
+    printf("ONSTART\n");
+    processImage(image);
+	//SDL_SaveBMP(image, "./ToGray.bmp");
+	return 0;
+}
