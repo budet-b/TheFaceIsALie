db41ea0e757b6bc1672eaa75e2a397c35f3007d9
Loic Reyreaud <loic.reyreaud@epita.fr>
2015-10-12 18:51:45 +0200

    HaarFixed

--- haar.c
+++ haar.c
@@ -60,6 +60,8 @@ void processImage(SDL_Surface *image) {
     int f = 0;
     int value;
     haarRecord *haarOutputTab;
+    haarRecord *haarOutput = NULL;
+    
     haarOutputTab = malloc((image->h*image->w)*20*(sizeof(struct haarRecord)));
    // display_matrix(image, integralImage);
     while((current_size < image->h) && (current_size < image->w)) {
@@ -70,18 +72,17 @@ void processImage(SDL_Surface *image) {
                     //printf("%d,%d,%d\n", i, j, current_size);
                     value = haarProcess(integralImage , i, j, current_size, n);
                     //printf("%d\n",value);
-                    if(value > 200000000)
+                    if(value > 0)
                     {
-                        haarRecord haarOutput;
-                        malloc(sizeof(struct haarRecord));
-                        haarOutput.value = value;
-                        haarOutput.haar = n;
-                        haarOutput.i = i;
-                        haarOutput.j = j;
-                        haarOutput.w = current_size;
-                        haarOutput.h = current_size;
+                        haarOutput = malloc(sizeof(struct haarRecord));
+                        haarOutput->value = value;
+                        haarOutput->haar = n;
+                        haarOutput->i = i;
+                        haarOutput->j = j;
+                        haarOutput->w = current_size;
+                        haarOutput->h = current_size;
                         //printf("Record %d done\n", f);
-                        haarOutputTab[f] = haarOutput;
+                        haarOutputTab[f] = *haarOutput;
                         free(haarOutput);
                         f = f + 1;
                     }
@@ -91,5 +92,5 @@ void processImage(SDL_Surface *image) {
         printf("MULT, %d\n",f);
         current_size *= 1.25;
     }
-    free(haarOutput);
+    free(haarOutputTab);
 }
