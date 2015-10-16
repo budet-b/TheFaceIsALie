afdf82252cc93de4b9e1e18c9e3d5456e57a2cce
Kais Bennaceur <kais.bennaceur@epita.fr>
2015-10-16 18:12:06 +0200

    Search function working :)

--- database/main.c
+++ database/main.c
@@ -5,28 +5,33 @@
 
 int main(int argc, char* argv[]) {
     FILE *name;
-    name = fopen ("name", "a+");
+    if((name = fopen("name", "r")) == NULL)
+        fprintf(stderr,"Impossible d'ouvrir le fichier données en lecture\n");
     int ch;
+    char h[100];
+    char *result = NULL;
+    int line = 0;
 
-    /*if( argc != 4 )
-        errx(2, "Insuffisant argument");*/
-    if( strcmp(argv[1], "add") == 0) {
-        fprintf(name, argv[2] );
+    if(argc < 2)
+        errx(2, "Insuffisant argument");
+    if(strcmp(argv[1], "add") == 0) {
+        fprintf(name,"%s",argv[2]);
         fprintf(name, " ");
-        fprintf(name, argv[3] );
+        fprintf(name,"%s", argv[3]);
         // rajouter les carac de la personne
         fprintf(name, "\n");
     }
 
     if( strcmp(argv[1], "read") == 0) {
-        while((ch = fgetc(name)) != EOF )
+        while((ch = fgetc(name)) != EOF)
             printf("%c", (char)ch);
     }
 
     if( strcmp(argv[1], "search") == 0) {
-        while((ch = fgetc(name)) != EOF) {
-            if(strcmp(argv[2], ch) == 0)
-                printf("ça marche");
+        while((fgets(h, 100, name) != NULL) && result == NULL) {
+            line++;
+            result = strstr(h, argv[2]);
+            printf("ça marche : %s Ligne : %d\n",result ,line);
         }
     }
 
