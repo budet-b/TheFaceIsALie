1c0c5dd865138825772f5c14f3fb2fb6049b98af
Kais Bennaceur <kais.bennaceur@epita.fr>
2015-11-01 19:55:02 +0100

    Database minus delete

--- database/main.c
+++ database/main.c
@@ -3,51 +3,96 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv[]) {
-    FILE *name;
-    if((name = fopen("name", "r")) == NULL)
-        fprintf(stderr,"Impossible d'ouvrir le fichier données en lecture\n");
+void add(int argc,char* argv[],FILE *name){
+    if(argc < 5) 
+        errx(2, "Insuffisant argument");
+    else
+        fprintf(name,"%s %s %s\n",argv[2],argv[3],argv[4]);
+}
+void read(FILE *name) {
     int ch;
+    while((ch = fgetc(name)) != EOF)
+        printf("%c", (char)ch);
+} 
+
+int search(char* argv[],FILE *name) {
     char h[100];
     char result[100];
+    int line = 0;
+    line++;
+    char *input = argv[2];
 
-    if(argc < 2)
-        errx(2, "Insuffisant argument");
-    if(strcmp(argv[1], "add") == 0) {
-        fprintf(name,"%s",argv[2]);
-        fprintf(name, " ");
-        fprintf(name,"%s", argv[3]);
-        // rajouter les carac de la personne
-        fprintf(name, "\n");
+    while((fgets(h, 100, name) != NULL) && result[0] == '\0') {
+        char *space = strchr(h, ' ');
+        char *dynh = h;
+        size_t length = space - dynh;
+        strncpy(result,dynh,length);
+            
+        if(strcmp(result,input)!=0)
+            memset(result, '\0',100);
+        else {
+            printf("Result : ");
+            for(space++;*space != '\0';space++)
+                printf("%c",*space);
+            break;
+        }
     }
+    if(result[0] == '\0') {
+        printf("Unrecognised haar feature : ");
+        printf("%s\n",argv[2]);   
+    }
+    return line;
+}
 
+
+void delete(char* argv[],FILE *name) {
+    int line = search(argv,name);
+    FILE *temp;
+    char h[100];
+    temp = fopen("temp","w+");
+    while((fgets(h, 100, name) != NULL)) {
+        line--;
+        if (line != 1) 
+            fputs(h,temp);
+    }
+    while(fgets(h,100,temp))
+        fputs(h,name);
+    fclose(temp);
+}
+
+int main(int argc, char* argv[]) {
+    FILE *name;
+    
+    if((fopen("name", "a+")) == NULL)
+        fprintf(stderr,"Impossible d'ouvrir le fichier données en lecture\n");
+    
+    if(argc < 2) 
+        errx(2, "Insuffisant argument");
+
+    if(strcmp(argv[1], "add") == 0){
+        name = fopen("name","a+");
+        add(argc, argv,name);
+        fclose(name);
+    }
+    
     if(strcmp(argv[1], "read") == 0) {
-        while((ch = fgetc(name)) != EOF)
-            printf("%c", (char)ch);
+        name = fopen("name","r");
+        read(name);
+        fclose(name);
     }
 
     if(strcmp(argv[1], "search") == 0) {
-        char *input = argv[2];
-        while((fgets(h, 100, name) != NULL) && result[0] == '\0') {
-            char *space = strchr(h, ' ');
-            char *dynh = h;
-            size_t length = space - dynh;
-            strncpy(result,dynh,length);
-            
-            if(strcmp(result,input)!=0)
-                memset(result, '\0',100);
-            else {
-                printf("Result : ");
-                for(space++;*space != '\0';space++)
-                    printf("%c",*space);
-                break;
-            }
-        }
-        if(result[0] == '\0') {
-            printf("Unrecognised haar feature : ");
-            printf("%s\n",argv[2]);   
-        }
+        name = fopen("name","r+");
+        search(argv,name);
+        fclose(name);
     }
+
+    if(strcmp(argv[1], "delete") == 0) {
+        name = fopen("name","r+");
+        delete(argv,name);
+        fclose(name);
+    }
+
     fclose(name);
     return 0;
 }
