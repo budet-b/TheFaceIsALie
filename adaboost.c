0b3b4998605f78bce1f87859fddd35e525d297d5
Loic Reyreaud <loic.reyreaud@epita.fr>
2015-11-11 13:08:13 +0100

    FIX WEIGHT

--- adaboost.c
+++ adaboost.c
@@ -36,7 +36,7 @@ int max(haarRecord* haarTab, int nbFeatures) {
 }
 
 double sum(int* visage, double* weights, int check, int nbFeatures) {
-    int sum = 0;
+    double sum = 0;
     for(int i = 0; i < nbFeatures; i++) {
         if (check == visage[i]) {
             sum = sum + weights[i];
@@ -45,15 +45,29 @@ double sum(int* visage, double* weights, int check, int nbFeatures) {
     return sum;
 }
 
+double sumWeight(double* weights, int nbExamples) {
+    double sum = 0;
+    for(int i = 0; i < nbExamples; i++)
+        sum = sum + weights[i];
+    return sum;
+}
+double* normalizeWeights(double* weights, int nbExamples) {
+    double sum = sumWeight(weights, nbExamples);
+    for(int i = 0; i < nbExamples; i++)
+        weights[i] = weights[i]/sum;
+    return weights;
+}
+
 double* weightInit(double* weights, int* visage, int nbExamplesNeg, int nbExamplesPos) {
     weights = malloc((nbExamplesPos + nbExamplesNeg) * sizeof(double));
     for(int i = 0; i < (nbExamplesPos + nbExamplesNeg); i++) {
         printf("assigning weight to example %d\n",i);
         if(visage[i] == -1)
-            weights[i] = (1/(2*nbExamplesNeg));
+            weights[i] = (0.5*(double)nbExamplesNeg);
         else
-            weights[i] = (1/(2*nbExamplesPos));
+            weights[i] = (0.5*(double)nbExamplesPos);
     }
+    weights = normalizeWeights(weights, nbExamplesNeg + nbExamplesPos);
     return weights;
 }
 
@@ -67,7 +81,7 @@ int applyWeakClassifier(weakClassifier* DS, char* image) {
     for(int i = 0; ; i++){
         if(compareHaar(haarTab[i], *DS)) { 
             if(DS->toggle == 1 && haarTab[i].value > DS->threshold) 
-                return DS->toggle;
+                return 1;
             else
                 return -1;
         }
@@ -80,23 +94,31 @@ void updateWeights(char* trainingExamples[], weakClassifier* DS,int* visage, dou
     for(int i = 0; i < nbExamples; i++) {
         printf("applying weak classifier to example %d\n",i);
         checksum = applyWeakClassifier(DS,trainingExamples[i]);
-        if(checksum != visage[i])
-            weights[i] = (weights[i]/2) * (1/DS->error);
-        else
-            weights[i] = (weights[i]/2) * (1/(1 - DS->error));
+        if(checksum == visage[i]) {
+            weights[i] = 1.0/(weights[i]*(DS->error));
+            printf("Classifier is wrong\n");
+        }
+        else{
+            weights[i] = 1.0/(weights[i]*(1.0 - DS->error));
+            printf("Classifier is right\n");
+        }
     }
+}
+
+void display_weights(double* weights, int* visage, int nbExamples) {
+    for(int i = 0; i < nbExamples; i++)
+        printf("weights: %f, visage: %d\n",weights[i], visage[i]); 
 }
 
 haarRecord** processMultipleImages(char* trainingExamples[], int nbExamples) {
     SDL_Surface** image_array = load_image_array(trainingExamples, nbExamples);
     haarRecord** haarTmp = malloc(nbExamples*sizeof(void*));
     haarRecord** haarOutput = malloc(162336*sizeof(void*));
-    haarOutput = malloc(162336*sizeof(void*));
     for(int i = 0; i < nbExamples; i++){
-        printf("mallocing examples %d\n",i);
-        haarOutput[i] = malloc(nbExamples*sizeof(haarRecord));
         haarTmp[i] = malloc(162336*sizeof(haarRecord));
-
+    }
+    for(int i = 0; i < 162336; i++) {
+        haarOutput[i] = malloc(nbExamples*sizeof(haarRecord));
     }
 
     int nbFeatures; // not necessary, always 162336
@@ -107,13 +129,13 @@ haarRecord** processMultipleImages(char* trainingExamples[], int nbExamples) {
     }
     for(int i = 0; i < nbExamples; i++) { // MEMORY EATER
         for(int j = 0; j < 162336; j++) {
-            printf("reversing %d,%d <- %d,%d\n",j,i,i,j);
+            //printf("reversing %d,%d <- %d,%d\n",j,i,i,j);
             haarOutput[j][i] = haarTmp[i][j];
         }
     }
     for(int i = 0; i < 162336; i++){
-        printf("sorting row %d\n",i);
-        sort(haarOutput[i], 162336); // sort each row, also 2nd MEMORY EATER
+        //printf("sorting row %d\n",i);
+        sort(haarOutput[i], nbExamples); // sort each row, also 2nd MEMORY EATER
     }
     free(haarTmp);
     return haarOutput;
@@ -143,12 +165,14 @@ weakClassifier* decisionStump (haarRecord *haarTab, int* visage, double* weights
     int toggle;
     int toggleTemp;
 
+
     //sumWeights init
     double WPosBig = sum(visage, weights, nbExamples, 1);
     double WNegBig = sum(visage, weights, nbExamples, -1);
     double WPosSmall = 0;
     double WNegSmall = 0;
-    
+
+
     //var init
     int j = 0;
     double errorPos;
@@ -170,11 +194,12 @@ weakClassifier* decisionStump (haarRecord *haarTab, int* visage, double* weights
             toggle = toggleTemp;
             margin = marginTemp;
             threshold = thresholdTemp;
-        }
-        if (j == nbExamples) {
             haarTmp = haarTab[j];
+        }
+        if (j == nbExamples - 1) {
             break;
         }
+        j++;
         while (1) {
             if (visage[j] == -1) { //updating weights balance with current treshold
                 WNegSmall = WNegSmall + weights[j];
@@ -184,14 +209,14 @@ weakClassifier* decisionStump (haarRecord *haarTab, int* visage, double* weights
                 WPosSmall = WPosSmall + weights[j];
                 WPosBig = WPosBig - weights[j];
             }
-            if (j == nbExamples || haarTab[j].value != haarTab[j+1].value) { //find new valid treshold
+            if (j == nbExamples - 1 || haarTab[j].value != haarTab[j+1].value) { //find new valid treshold
                 break;
             }
             else {
                 j++;
             }
         }
-        if (j == nbExamples) {
+        if (j == nbExamples - 1) {
             thresholdTemp = haarTab[nbExamples - 1].value + 1;
             marginTemp = 0;
         }
@@ -200,7 +225,7 @@ weakClassifier* decisionStump (haarRecord *haarTab, int* visage, double* weights
             marginTemp = haarTab[j+1].value - haarTab[j].value;
         }
     }
-    weakClassifier* bestWeak = NULL;
+    weakClassifier* bestWeak = malloc(sizeof(struct weakClassifier));
     bestWeak->f = haarTmp;
     bestWeak->threshold = threshold;
     bestWeak->toggle = toggle;
@@ -211,13 +236,15 @@ weakClassifier* decisionStump (haarRecord *haarTab, int* visage, double* weights
 
 weakClassifier* bestStump (haarRecord** haarFeatures, int* visage, double* weights, int nbExamples){
     weakClassifier* currentDS;
-    weakClassifier* bestDS = NULL;
+    weakClassifier *bestDS = malloc(sizeof(struct weakClassifier));
     bestDS->threshold = 0;
     bestDS->toggle = 0;
     bestDS->error = 2;
     bestDS->margin = 0;
     for (int f = 0; f < 162336; f++) {
+        //printf("Working on feature %d\n",f);
         currentDS = decisionStump(haarFeatures[f], visage, weights, nbExamples);
+        //printf("CurrentDS:\n\t error: %d \n\t threshold: %d \n", currentDS->error, currentDS->threshold);
          if ((currentDS->error < bestDS->error) || ((currentDS->error == bestDS->error) && (currentDS->margin > bestDS->margin)))
             bestDS = currentDS;
     }
@@ -233,15 +260,17 @@ strongClassifier* adaboost (char* trainingExamples[], int* visage, int visagePos
     double alpha;
     struct haarRecord** haarFeatures;
     struct weakClassifier *currentDS = NULL;
-    weightInit(weights, visage, visagePos, visageNeg);
+    weights = weightInit(weights, visage, visagePos, visageNeg);
     haarFeatures = processMultipleImages(trainingExamples, nbExamples);
 
     for (int i = 0; i < trainingRound; i++) {
+        display_weights(weights, visage, nbExamples);
         printf("round %d\n",i);
         currentDS = bestStump(haarFeatures, visage, weights, nbExamples);
         printf("processing alpha\n");
         alpha = (1/2)*log((1 - currentDS->error)/currentDS->error);
         updateWeights(trainingExamples, currentDS, visage, weights, nbExamples);
+        weights = normalizeWeights(weights, nbExamples);
         printf("adding weak classifier\n");
         result[i].alpha = alpha;
         result[i].classifier = *currentDS;
